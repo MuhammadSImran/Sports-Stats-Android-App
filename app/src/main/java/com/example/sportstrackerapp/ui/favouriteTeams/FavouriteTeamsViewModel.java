bc5eb0d94df79561efe46a4bb22993f7583a962e
thatboyychidex <chuk7180@mylaurier.ca>
2024-10-30 21:58:32 -0400

    Update FavouriteTeamsViewModel.java

--- app/src/main/java/com/example/sportstrackerapp/ui/favouriteTeams/FavouriteTeamsViewModel.java
+++ app/src/main/java/com/example/sportstrackerapp/ui/favouriteTeams/FavouriteTeamsViewModel.java
@@ -4,16 +4,44 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.sportstrackerapp.ui.favouriteTeams.models.Team;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FavouriteTeamsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<Team>> favouriteTeams;
 
     public FavouriteTeamsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        favouriteTeams = new MutableLiveData<>(new ArrayList<>());
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<Team>> getFavouriteTeams() {
+        return favouriteTeams;
+    }
+
+    public void addTeam(Team team) {
+        List<Team> currentTeams = favouriteTeams.getValue();
+        if (currentTeams != null) {
+            currentTeams.add(team);
+            favouriteTeams.setValue(currentTeams);
+        }
+    }
+
+    public void removeTeam(Team team) {
+        List<Team> currentTeams = favouriteTeams.getValue();
+        if (currentTeams != null) {
+            currentTeams.remove(team);
+            favouriteTeams.setValue(currentTeams);
+        }
+    }
+
+    public void clearAllTeams() {
+        List<Team> currentTeams = favouriteTeams.getValue();
+        if (currentTeams != null) {
+            currentTeams.clear();
+            favouriteTeams.setValue(currentTeams);
+        }
     }
 }
