a6a0dfa65fa36d5ec5b65a6baaf2b8fed585bf5e
Ethan Katsiroubas <136759655+EthanKatsi@users.noreply.github.com>
2024-11-18 16:01:09 -0500

    Replaced HighlightsFragment & HighlightsViewModel
    Reset HighlightsFragment and HighlightsViewModel so there's no error when running the app.

--- app/src/main/java/com/example/sportstrackerapp/ui/highlights/HighlightsFragment.java
+++ app/src/main/java/com/example/sportstrackerapp/ui/highlights/HighlightsFragment.java
@@ -1,24 +1,37 @@
-package com.example.highlightsofsportapp;
+package com.example.sportstrackerapp.ui.highlights;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.sportstrackerapp.databinding.FragmentHighlightsBinding;
+
+public class HighlightsFragment extends Fragment {
+
+    private FragmentHighlightsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        com.example.sportstrackerapp.ui.highlights.HighlightsViewModel HighlightsViewModel =
+                new ViewModelProvider(this).get(com.example.sportstrackerapp.ui.highlights.HighlightsViewModel.class);
+
+        binding = FragmentHighlightsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHighlights;
+        HighlightsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
