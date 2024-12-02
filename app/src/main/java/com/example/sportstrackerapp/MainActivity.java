49a24f417cf2acbcadd40ea76b8e1abccec769f4
Ethan Katsiroubas <136759655+EthanKatsi@users.noreply.github.com>
2024-12-01 23:31:09 -0500

    updated mainactivity.java

--- app/src/main/java/com/example/sportstrackerapp/MainActivity.java
+++ app/src/main/java/com/example/sportstrackerapp/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.sportstrackerapp;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.example.sportstrackerapp.databinding.ActivityMainBinding;
+import com.example.sportstrackerapp.ui.news.NewsFragment;
+import com.example.sportstrackerapp.ui.profile.ProfileFragment;
+import com.example.sportstrackerapp.ui.scores.ScoresFragment;
+import com.example.sportstrackerapp.ui.standings.StandingsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,5 +38,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        if (!navController.popBackStack()) {
+            super.onBackPressed();
+        }
     }
 }
